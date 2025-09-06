baab642e1b7d07caa57dcde9ee8021e84dc03f2e
MUNACHIW <munasom25@gmail.com>
2025-09-06 23:05:28 +0100

    is not an easy work

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,7 +40,8 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    "jazzmin",
+    # "jazzmin",
+    "jet",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
