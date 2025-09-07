60065db493904941a87203ed28a06fd85aa8b1b8
MUNACHIW <munasom25@gmail.com>
2025-09-07 01:09:48 +0100

    debug false

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = False
 
 # Updated for deployment
 ALLOWED_HOSTS = [
